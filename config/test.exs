24097831b602b1f3ff4ad381ae41a66fafacfc46
Dylan Perry <dylanbperry@gmail.com>
2024-06-26 16:31:46 -0700

    Update username and password for test config to utilize local user name instead of postgres

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :elix_library_eshop, ElixLibraryEshop.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "dylan",
+  password: "dylan",
   hostname: "localhost",
   database: "elix_library_eshop_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
