8acdd928ea13d8c7936e6e43af752066fa43d606
Dylan Perry <dylanbperry@gmail.com>
2024-07-01 13:59:24 -0700

    Add basic view to page and controller

--- lib/elix_library_eshop_web/controllers/page_controller.ex
+++ lib/elix_library_eshop_web/controllers/page_controller.ex
@@ -8,6 +8,19 @@ defmodule ElixLibraryEshopWeb.PageController do
   end
 
   def about(conn, _params) do
+
+    # Multi-assign, Option 1: chained pipes
+    conn = conn
+          |> assign(:message, "This string was passed with a controller variable.")
+          |> assign(:other_message, "Other message")
+
+    # # Multi-assign, Option 2: merge_assigns function
+    # conn = merge_assigns(conn, message: "First message.", other_message: "Other message.")
+
+    # # Multi-assign, Option 3: pass keyword list directly to render function
+    # render(conn, :about, message: "Rendered first message.", other_message: "Rendered other message.")
+
+    # Standard render response
     render(conn, :about)
   end
 end
