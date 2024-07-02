2e48156b87a3a2f6b7ae61ba71873c12c10bf953
Dylan Perry <dylanbperry@gmail.com>
2024-07-02 11:59:33 -0700

    Add helper function book_inserted_at to book_html.ex

--- lib/elix_library_eshop_web/controllers/book_html.ex
+++ lib/elix_library_eshop_web/controllers/book_html.ex
@@ -3,4 +3,10 @@ defmodule ElixLibraryEshopWeb.BookHTML do
 
   embed_templates "/book_html/*"
 
+  alias ElixLibraryEshop.Books.Book
+
+  def book_inserted_at(%Book{inserted_at: timestamp}) do
+    Calendar.strftime(timestamp, "%m/%d/%Y %I:%M%p")
+  end
+
 end
